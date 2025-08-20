5da0cac771eaa054577e9bd78634d3b3e19134f3
adusumillipraveen <praveen.adusumilli@hmcts.net>
2025-08-20 12:30:06 +0100

    fix azurerm 3

--- servicebus.tf
+++ servicebus.tf
@@ -10,10 +10,8 @@ data "azurerm_servicebus_topic" "ccd_case_events_topic" {
 }
 
 resource "azurerm_servicebus_topic_authorization_rule" "ccd_case_events_topic_send" {
-  name                = "${var.product}-sendonly"
-  namespace_name      = data.azurerm_servicebus_namespace.ccd_namespace.name
-  topic_name          = data.azurerm_servicebus_topic.ccd_case_events_topic.name
-  resource_group_name = data.azurerm_servicebus_namespace.ccd_namespace.resource_group_name
+  name     = "${var.product}-sendonly"
+  topic_id = data.azurerm_servicebus_topic.ccd_case_events_topic.id
 
   listen = false
   send   = true
@@ -22,7 +20,7 @@ resource "azurerm_servicebus_topic_authorization_rule" "ccd_case_events_topic_se
 
 resource "azurerm_key_vault_secret" "ccd_case_events_send_primary_connection_string" {
   name         = "ccd-case-events-sendonly-connection-string"
-  value       = azurerm_servicebus_topic_authorization_rule.ccd_topic_send.primary_connection_string
+  value        = azurerm_servicebus_topic_authorization_rule.ccd_case_events_topic_send.primary_connection_string
   key_vault_id = module.key-vault.key_vault_id
 
   content_type = "terraform-managed"
@@ -30,4 +28,3 @@ resource "azurerm_key_vault_secret" "ccd_case_events_send_primary_connection_str
     "source" : "ccd-case-events ${var.product}-sendonly"
   })
 }
-
