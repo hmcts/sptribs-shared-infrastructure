8eaf928bbeb154fc0a899088f09e596b315048bf
EllisD-B <ellis.domengerboyce@hmcts.net>
2025-09-15 08:37:44 +0100

    postgres server config

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 
 provider "azurerm" {
   features {}
-  alias           = "sptribs_vnet"
+  alias           = "postgres_network"
   subscription_id = var.aks_subscription_id
 }
 
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "rg" {
 # FlexiServer v15
 module "db-v15" {
   providers = {
-    azurerm.postgres_network = azurerm.sptribs_vnet
+    azurerm.postgres_network = azurerm.postgres_network
   }
 
   source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
@@ -46,7 +46,27 @@ module "db-v15" {
     {
       name  = "azure.extensions"
       value = "pg_stat_statements,pg_buffercache"
-    }
+    },
+    {
+      name  = "log_lock_waits"
+      value = "on"
+    },
+    {
+      name  = "pg_qs.query_capture_mode"
+      value = "ALL"
+    },
+    {
+      name  = "pgms_wait_sampling.query_capture_mode"
+      value = "ALL"
+    },
+    {
+      name  = "logfiles.download_enable"
+      value = "ON"
+    },
+    {
+      name  = "logfiles.retention_days"
+      value = "14"
+    },
   ]
 
   pgsql_sku                      = var.pgsql_sku
