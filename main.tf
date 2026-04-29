001607dbe9e41946b309b491d3fd7a9fe87cf37f
manohar.nunna <manohar.nunna@hmcts.net>
2026-04-29 09:24:05 +0100

    DTSPO-30514: Add Jenkins MI keyvault access

--- main.tf
+++ main.tf
@@ -12,11 +12,13 @@ provider "azurerm" {
   subscription_id = var.aks_subscription_id
 }
 
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
   tags     = var.common_tags
 }
-
-
-
